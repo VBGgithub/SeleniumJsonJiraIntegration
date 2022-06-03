6993931aa8f74adb1713c3c0973b3a80be63feae
vrinda <vrindajagtap@gmail.com>
2022-06-03 21:01:30 +0530

    first commit

---
+++ src/main/java/com/demo/configuration/Configuration.java
@@ -0,0 +1,14 @@
+package com.demo.configuration;
+
+public class Configuration {
+	public static String urlJira="https://vbgunjal.atlassian.net/";
+	public static String host="vbgunjal.atlassian.net";
+	public static String url="https://opensource-demo.orangehrmlive.com";
+	public static String jsonCreatePath="I:\\IMP Backup\\05_Vrinda Office data\\Cogniwise\\Training\\New Eclipse\\Projects\\SeleniumJiraJson\\createIssue.json";
+	public static String jsonEditPath="I:\\IMP Backup\\05_Vrinda Office data\\Cogniwise\\Training\\New Eclipse\\Projects\\SeleniumJiraJson\\editIssue.json";
+	public static String browser="chrome";
+	public static String browserPath="I:\\IMP Backup\\05_Vrinda Office data\\Cogniwise\\Training\\Drivers\\chromedriver.exe";
+	public static String email="vrindagunjal@gmail.com";
+	public static String password="NV5a7BgX2g4oDD7hrFoz9E63";
+	public static String project="TF";
+}
